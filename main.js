dc1d3af7834ea78213963c62c5a5b476e2d77fbf
Giniaux Matteo <Matteo.GINIAUX@etu.uca.fr>
2024-12-16 19:24:37 +0100

    Last Change A recup

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const hbs = require("hbs");
 const path = require('path');
 
 const app = express();
-//const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 const PORT = 3008;
 
 
@@ -35,63 +35,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-async function seed() {
-    try {
-        // Create some genres
-        const genre1 = await prisma.genre.create({
-            data: {
-                name: "Action",
-            },
-        });
-        const genre2 = await prisma.genre.create({
-            data: {
-                name: "Adventure",
-            },
-        });
-        // Create some publishers
-        const publisher1 = await prisma.gamePublishers.create({
-            data: {
-                publisher: "Ubisoft",
-            },
-        });
-        const publisher2 = await prisma.gamePublishers.create({
-            data: {
-                publisher: "Nintendo",
-            },
-        });
-        // Create some games
-        await prisma.games.createMany({
-            data: [
-                {
-                    title: "Assassin's Creed",
-                    description: "An action-adventure stealth game.",
-                    releaseDate: new Date("2007-11-13"),
-                    genreId: genre1.id,
-                    publishersId: publisher1.id,
-                },
-                {
-                    title: "The Legend of Zelda",
-                    description: "An action-adventure game series.",
-                    releaseDate: new Date("1986-02-21"),
-                    genreId: genre2.id,
-                    publishersId: publisher2.id,
-                },
-                {
-                    title: "Far Cry",
-                    description: "A first-person shooter game.",
-                    releaseDate: new Date("2004-03-23"),
-                    genreId: genre1.id,
-                    publishersId: publisher1.id,
-                },
-            ],
-        });
-        console.log("Database seeded successfully!");
-    } catch (error) {
-        console.error("Error seeding database:", error.message);
-    } finally {
-        await prisma.$disconnect();
-    }
-}
-seed();
